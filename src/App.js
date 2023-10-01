c639433f53d181565d014f68bf10f7f25ef548f8
mustafaahmed-f <mostafafikry97@gmail.com>
2023-10-01 17:32:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { skillsArr } from "./skills.js";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProfileImage />
+      <Info />
+    </div>
+  );
+}
+
+function ProfileImage() {
+  return (
+    <img
+      alt="ProfileImage"
+      src="WhatsApp Image 2023-05-07 at 2.54.32 PM.jpeg"
+    ></img>
+  );
+}
+
+function Info() {
+  return (
+    <div className="info">
+      <Bio />
+      <Skills />
+    </div>
+  );
+}
+
+function Bio() {
+  return (
+    <div>
+      <h1>Mustafa Ahmed</h1>
+      <p>
+        MERN stack developer skilled with Js , HTML , css , react & NodeJs .
+        Seeking for a stable position in a company where I can use my skills.
+      </p>
+    </div>
+  );
+}
+
+function Skills() {
+  // const mySkills = skillsObj;
+
+  return (
+    <div className="skills-section">
+      {skillsArr.map((skillObj) => {
+        return <Skill skills={skillObj} key={skillObj.skill} />;
+      })}
+    </div>
+  );
+}
+
+function Skill({ skills }) {
+  let level = null;
+  switch (skills.level) {
+    case "beginner":
+      level = "👶";
+      break;
+    case "intermediate":
+      level = "👍";
+      break;
+    case "advanced":
+      level = "💪";
+      break;
+
+    default:
+      break;
+  }
+
+  return (
+    <div className="skills" style={{ backgroundColor: skills.color }}>
+      <h6>{skills.skill}</h6>
+      <span>{level}</span>
     </div>
   );
 }
---
+++ src/skills.js
@@ -0,0 +1,32 @@
+export const skillsArr = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "beginner",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Node.Js",
+    level: "intermediate",
+    color: "#008000",
+  },
+];
